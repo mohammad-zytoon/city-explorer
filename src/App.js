c27ab66bb677d5a38bd7340a8a0c54a2e266ac9b
mohammad-zytoon <mohamadzaiton@gmail.com>
2021-05-23 19:27:43 +0300

    lab06 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      quearySearch: '',
+      locatData: '',
+      displayMap: false,
+      errorMessage: false,
+      theError: ''
+    }
+  }
+
+  getPlace = async (event) => {
+    event.preventDefault();
+
+    let placeUrl = `https://eu1.locationiq.com/v1/search.php?key=pk.2b421377eaad8060e237f29dd060432b&q=${this.state.quearySearch}&format=json`;
+
+    try {
+
+      let placeResult = await axios.get(placeUrl);
+
+      this.setState({
+        locatData: placeResult.data[0],
+        displayMap: true,
+        errorMessage: false
+      })
+    }
+    catch (error) {
+      this.setState({
+        displayMap: false,
+        errorMessage: true,
+        theError: error
+      })
+    }
+  }
+
+  updateQuearySearch = (event) => {
+    this.setState({
+      quearySearch: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <h1>City Explorer</h1>
+        <form onSubmit={this.getPlace}>
+          <input type='text' placeholder='add a city' onChange={this.updateQuearySearch} />
+          <button>Explore</button>
+          <input type='submit' value='Get Location' />
+        </form> */}
+
+        <Form onSubmit={this.getPlace}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="text" placeholder="Insert City " onChange={this.updateQuearySearch} />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Explore
+          </Button>
+        </Form>
+
+        
+        {this.state.displayMap &&
+
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=pk.2b421377eaad8060e237f29dd060432b&center=${this.state.locatData.lat},${this.state.locatData.lon}`} />
+            <Card.Body>
+              <Card.Title>{this.state.locatData.display_name}</Card.Title>
+              <Card.Text>
+                {this.state.locatData.lat} 
+                {this.state.locatData.lon}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        }
+
+        {this.state.errorMessage &&
+
+          <Alert variant="danger">
+            Try Another Entry, Error:
+        {this.state.theError.response.status}
+          </Alert>
+        }
+      </div>
+
+    );
+  }
 }
 
 export default App;
